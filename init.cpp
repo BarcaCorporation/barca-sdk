47c9244ee73e3ae49bc309cf830454f1387cb24a
ahsouza <annibalhsouza@gmail.com>
2022-03-03 19:28:38 -0300

    feat: implementing basic method for user authentication

--- init.cpp
+++ init.cpp
@@ -1,11 +1,42 @@
 #include <iostream>
 #include <string>
 #include <cstdlib>
+#include<conio.h>
+
 using namespace std;
 
 int option;
+const string ROOT_LOGIN = "root";
+const string ROOT_PASSWORD = "12345";
 
 int main() {
+  int index = 0;
+  char login[20];
+  char password[128], c;
+
+  printf("login: ");
+  scanf("%19s", &login);
+  
+   printf("senha: ");
+   while((c = getch()) != 13){
+       if(index < 0)
+           index = 0;
+       /* 8 is ASCII value of BACKSPACE character */
+       if(c == 8){
+           putch('\b');
+           putch('\b');
+           index--;
+       continue;
+       }
+       password[index++] = c;
+       putch('*');
+   }
+   password[index] = '\0';
+  // printf("\n\nsenha: %s", password);
+
+  if (login == ROOT_LOGIN && password == ROOT_PASSWORD) {
+    printf("\n\n\n*  Welcome To BWC! \n\nvocê está logado como %s\n", login); 
+
     cout << "Qual ferramenta deseja instalar?\n\n[1] Barca CLI\n[2] Barca Web Cloud\n[3] Mamute Security\n\n\n[0]Sair" << endl;
     cin >> option;
     switch (option) {
@@ -18,7 +49,8 @@ int main() {
         system("pause");
         break;
       case 3:
-        cout << "Instalando Mamute Security..." << endl;
+        printf("\n\n\n*  Welcome To BWC! \n\nvocê está logado como %s\n", login);
+
         system("pause");
         break;
       case 0:
@@ -29,6 +61,12 @@ int main() {
         system("pause");
         break;
     }
-    
+  } else {
+    printf("\n\nAutenticacao falhou");
+    system("pause");
     return 0;
+  }
+  system("pause");
+    
+  return 0;
 }
