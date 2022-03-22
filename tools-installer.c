a84ea2d8fb99c6d1a38c88cd53dd3133ff7472ac
ahsouza <annibalhsouza@gmail.com>
2022-03-22 12:26:47 -0300

    feat: implementation latin characters with utf 8 code and start creating folders and subfolders

--- tools-installer.c
+++ tools-installer.c
@@ -1,6 +1,8 @@
+#define _UNIX03_SOURCE
 #include <Windows.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <locale.h>
 
 void initSDK() {
   if(!CreateDirectory("C:\\Barca", NULL)) {
@@ -24,18 +26,58 @@ void initDesktop() {
   }
 }
 
-void initBWC() {
-  char *profile;
+void initAPIs() {
+  char *programFiles;
+  programFiles = getenv("ProgramFiles");
+  printf(programFiles);
+}
+
+void initBWC(void) {
+  char* profile;
   profile = getenv("USERPROFILE");
 
-  printf(profile);
-  if(!CreateDirectory("C:\\.bwc", NULL)) {
+  if(!CreateDirectory(*profile+"\\.bwc", NULL)) {
     fprintf(stderr, "ERRO: %d\n", GetLastError());
   }
+  system("setx BARCA_CLOUD \"C:\\.bwc\"");
+
 }
 
 int main() {
+  setlocale(LC_ALL, "Portuguese_Brasil");
   initSDK();
- 
+  
+  if(!CreateDirectory("C:\\Barca\\lib", NULL)) {
+    fprintf(stderr, "ERRO: %d\n", GetLastError());
+  }
+  if(!CreateDirectory("C:\\Barca\\bin", NULL)) {
+    fprintf(stderr, "ERRO: %d\n", GetLastError());
+  }
+  if(!CreateDirectory("C:\\Barca\\etc", NULL)) {
+    fprintf(stderr, "ERRO: %d\n", GetLastError());
+  }
+  if(!CreateDirectory("C:\\Barca\\log", NULL)) {
+    fprintf(stderr, "ERRO: %d\n", GetLastError());
+  }
+  if(!CreateDirectory("C:\\Barca\\engine", NULL)) {
+    fprintf(stderr, "ERRO: %d\n", GetLastError());
+  }
+  if(!CreateDirectory("C:\\Barca\\engine\\core", NULL)) {
+    fprintf(stderr, "ERRO: %d\n", GetLastError());
+  }
+  if(!CreateDirectory("C:\\Barca\\engine\\modules", NULL)) {
+    fprintf(stderr, "ERRO: %d\n", GetLastError());
+  }
+  if(!CreateDirectory("C:\\Barca\\engine\\docs", NULL)) {
+    fprintf(stderr, "ERRO: %d\n", GetLastError());
+  }
+  if(!CreateDirectory("C:\\Barca\\engine\\config", NULL)) {
+    fprintf(stderr, "ERRO: %d\n", GetLastError());
+  }
+  if(!CreateDirectory("C:\\Barca\\engine\\tracking", NULL)) {
+    fprintf(stderr, "ERRO: %d\n", GetLastError());
+  }
+  
+  
   return 0;
 }
