7a0a31d10bbd8b8b604339e769d0ff2611627f77
ahsouza <annibalhsouza@gmail.com>
2022-03-22 16:58:32 -0300

    feat: add method to generate logs

--- tools-installer.c
+++ tools-installer.c
@@ -4,8 +4,9 @@
 #include <stdlib.h>
 #include <locale.h>
 
+FILE *file;
+
 void initSDK() {
-  FILE *file;
 
   if(!CreateDirectory("C:\\Barca", NULL)) {
     fprintf(stderr, "ERRO: %d\n", GetLastError());
@@ -31,6 +32,9 @@ void initSDK() {
   if(!CreateDirectory("C:\\Barca\\engine\\modules", NULL)) {
     fprintf(stderr, "ERRO: %d\n", GetLastError());
   }
+  if(!CreateDirectory("C:\\Barca\\engine\\modules\\apis", NULL)) {
+    fprintf(stderr, "ERRO: %d\n", GetLastError());
+  }
   if(!CreateDirectory("C:\\Barca\\engine\\docs", NULL)) {
     fprintf(stderr, "ERRO: %d\n", GetLastError());
   }
@@ -44,7 +48,7 @@ void initSDK() {
     fprintf(stderr, "ERRO: %d\n", GetLastError());
   }
 
-  file = fopen("C:\\Barca\\engine\\version.txt", "w+");
+  file = fopen("C:\\Barca\\engine\\version.txt", "w");
   file = fopen("C:\\Barca\\engine\\serial.txt", "w+");
   fputs("----------------------------------------------------------\n", file);
   fputs("                                                          \n", file);
@@ -56,6 +60,11 @@ void initSDK() {
   fputs("----------------------------------------------------------\n\n\n", file);
   fputs("SERIAL KEY:  YKUOR1-AOFLR9-APTROD-MBUSO4-OPR301", file);
 
+}
+
+void iLog(char* path, char* txt) {
+  file = fopen(path, "w");
+  fputs(txt, file);
 }
 
 void initCLI() {
@@ -93,7 +102,9 @@ void initBWC(void) {
 
 int main() {
   setlocale(LC_ALL, "Portuguese_Brasil");
+  
   initSDK();
+  iLog("C:\\Barca\\logs\\log-march-2022.txt", "LOG  22/03/2022 -  Instalação Concluída!");
  
   return 0;
 }
