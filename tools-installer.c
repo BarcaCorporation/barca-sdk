ceda3734079a49c6666be56ed8cc54adb0a43920
ahsouza <annibalhsouza@gmail.com>
2022-03-22 16:25:32 -0300

    feat: add automation in the generation of files and folders for the barca engine

--- tools-installer.c
+++ tools-installer.c
@@ -5,9 +5,57 @@
 #include <locale.h>
 
 void initSDK() {
+  FILE *file;
+
   if(!CreateDirectory("C:\\Barca", NULL)) {
     fprintf(stderr, "ERRO: %d\n", GetLastError());
   }
+  if(!CreateDirectory("C:\\Barca\\lib", NULL)) {
+    fprintf(stderr, "ERRO: %d\n", GetLastError());
+  }
+  if(!CreateDirectory("C:\\Barca\\bin", NULL)) {
+    fprintf(stderr, "ERRO: %d\n", GetLastError());
+  }
+  if(!CreateDirectory("C:\\Barca\\etc", NULL)) {
+    fprintf(stderr, "ERRO: %d\n", GetLastError());
+  }
+  if(!CreateDirectory("C:\\Barca\\log", NULL)) {
+    fprintf(stderr, "ERRO: %d\n", GetLastError());
+  }
+  if(!CreateDirectory("C:\\Barca\\engine", NULL)) {
+    fprintf(stderr, "ERRO: %d\n", GetLastError());
+  }
+  if(!CreateDirectory("C:\\Barca\\engine\\core", NULL)) {
+    fprintf(stderr, "ERRO: %d\n", GetLastError());
+  }
+  if(!CreateDirectory("C:\\Barca\\engine\\modules", NULL)) {
+    fprintf(stderr, "ERRO: %d\n", GetLastError());
+  }
+  if(!CreateDirectory("C:\\Barca\\engine\\docs", NULL)) {
+    fprintf(stderr, "ERRO: %d\n", GetLastError());
+  }
+  if(!CreateDirectory("C:\\Barca\\engine\\config", NULL)) {
+    fprintf(stderr, "ERRO: %d\n", GetLastError());
+  }
+  if(!CreateDirectory("C:\\Barca\\engine\\tracking", NULL)) {
+    fprintf(stderr, "ERRO: %d\n", GetLastError());
+  }
+  if(!CreateDirectory("C:\\Barca\\cli", NULL)) {
+    fprintf(stderr, "ERRO: %d\n", GetLastError());
+  }
+
+  file = fopen("C:\\Barca\\engine\\version.txt", "w+");
+  file = fopen("C:\\Barca\\engine\\serial.txt", "w+");
+  fputs("----------------------------------------------------------\n", file);
+  fputs("                                                          \n", file);
+  fputs("                                                          \n", file);
+  fputs("               Copyright © Project Barca                  \n", file);
+  fputs("                                                          \n", file);
+  fputs("                                                          \n", file);
+  fputs("  Email: annibalhsouza@gmail.com    Author: Aníbal Souza  \n", file);
+  fputs("----------------------------------------------------------\n\n\n", file);
+  fputs("SERIAL KEY:  YKUOR1-AOFLR9-APTROD-MBUSO4-OPR301", file);
+
 }
 
 void initCLI() {
@@ -46,38 +94,6 @@ void initBWC(void) {
 int main() {
   setlocale(LC_ALL, "Portuguese_Brasil");
   initSDK();
-  
-  if(!CreateDirectory("C:\\Barca\\lib", NULL)) {
-    fprintf(stderr, "ERRO: %d\n", GetLastError());
-  }
-  if(!CreateDirectory("C:\\Barca\\bin", NULL)) {
-    fprintf(stderr, "ERRO: %d\n", GetLastError());
-  }
-  if(!CreateDirectory("C:\\Barca\\etc", NULL)) {
-    fprintf(stderr, "ERRO: %d\n", GetLastError());
-  }
-  if(!CreateDirectory("C:\\Barca\\log", NULL)) {
-    fprintf(stderr, "ERRO: %d\n", GetLastError());
-  }
-  if(!CreateDirectory("C:\\Barca\\engine", NULL)) {
-    fprintf(stderr, "ERRO: %d\n", GetLastError());
-  }
-  if(!CreateDirectory("C:\\Barca\\engine\\core", NULL)) {
-    fprintf(stderr, "ERRO: %d\n", GetLastError());
-  }
-  if(!CreateDirectory("C:\\Barca\\engine\\modules", NULL)) {
-    fprintf(stderr, "ERRO: %d\n", GetLastError());
-  }
-  if(!CreateDirectory("C:\\Barca\\engine\\docs", NULL)) {
-    fprintf(stderr, "ERRO: %d\n", GetLastError());
-  }
-  if(!CreateDirectory("C:\\Barca\\engine\\config", NULL)) {
-    fprintf(stderr, "ERRO: %d\n", GetLastError());
-  }
-  if(!CreateDirectory("C:\\Barca\\engine\\tracking", NULL)) {
-    fprintf(stderr, "ERRO: %d\n", GetLastError());
-  }
-  
-  
+ 
   return 0;
 }
